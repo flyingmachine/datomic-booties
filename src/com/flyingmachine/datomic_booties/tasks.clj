c1b363b0daafce3719c1952e31d9e1061dabe748
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2016-04-30 19:20:21 -0400

    task descriptions

--- src/com/flyingmachine/datomic_booties/tasks.clj
+++ src/com/flyingmachine/datomic_booties/tasks.clj
@@ -7,8 +7,9 @@
     [com.flyingmachine.datomic-booties.core :as bd]))
 
 (defmacro defdbtask
-  [name & body]
+  [name desc & body]
   `(deftask ~name
+     ~desc
      ~'[u uri VAL str "Datomic URI"]
      ~'(if-not uri
          (do (util/fail "The -u/--uri option is required!") (*usage*)))
@@ -16,18 +17,23 @@
      identity))
 
 (defdbtask migrate-db
+  "Conform schema and fixtures"
   (bd/conform (d/connect uri)))
 
 (defdbtask create-db
+  "Create datomic db"
   (d/create-database uri))
 
 (defdbtask delete-db
+  "Delete datomic db"
   (d/delete-database uri))
 
 (defdbtask bootstrap-db
+  "Create and migrate db"
   (comp (create-db  :uri uri)
         (migrate-db :uri uri)))
 
 (defdbtask recreate-db
+  "Delete then bootstrap db"
   (comp (delete-db    :uri uri)
         (bootstrap-db :uri uri)))
